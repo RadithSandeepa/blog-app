f0f0009501191fedd15ce8b39b0bdbb2fb158d3f
RadithSandeepa <radithsandeepa500@gmail.com>
2024-07-14 22:21:19 +0530

    Implemented AuthContext

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
+import { AuthContext } from '../context/authContext';
 import logo from '../img/logo.png';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const { currentuser, logout } = useContext(AuthContext);
+
+
   return (
     <div className='navbar'>
         <div className="container">
@@ -15,7 +20,7 @@ const Navbar = () => {
               <Link className="link" to="/?cat=cinema"><h6>CINEMA</h6></Link>
               <Link className="link" to="/?cat=design"><h6>DESIGN</h6></Link>
               <Link className="link" to="/?cat=food"><h6>FOOD</h6></Link>
-              <span>John</span>
+              <span>{currentuser?.username}</span>
               <span>Logout</span>
               <span className='write'>
                 <Link to="/write" className='link'>Write</Link>
