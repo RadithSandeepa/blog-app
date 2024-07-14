0e058b9e2bd74a5b1f4f9578d042237fb512e1c0
RadithSandeepa <radithsandeepa500@gmail.com>
2024-07-14 19:53:26 +0530

    Implemented Register function in both backend and frontend

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
 import express from "express";
-import dotenv from 'dotenv';
-
-dotenv.config();
-
+import postRoute from './routes/posts.js';
+import authRoute from './routes/auth.js';
+import userRoute from './routes/users.js';
 
 const app = express();
 
-
 //middlewares
 app.use(express.json());
+app.use("/api/posts", postRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
     console.log("Connected to server");
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+    setInputs( prev=>({...prev, [e.target.name]: e.target.value}) )
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    try{
+      const res = await axios.post("/auth/register", inputs);
+      console.log(res);
+    }catch(err){
+      console.log(err);
+    }
+    
+  }
+  
   return (
     <div className='auth'>
         <h1>Register</h1>
         <form>
-            <input type="text" placeholder='username' required/>
-            <input type="email" placeholder='email' required/>
-            <input type="password" placeholder='password' required/>
-            <button>Register</button>
+            <input type="text" placeholder='username' name='username' onChange={handleChange} required/>
+            <input type="email" placeholder='email' name='email' onChange={handleChange} required/>
+            <input type="password" placeholder='password' name='password' onChange={handleChange} required/>
+            <button onClick={handleSubmit}>Register</button>
             <p>This is an error!</p>
             <span>Already have an account? <Link to="/login">Sign in</Link></span>
         </form>
